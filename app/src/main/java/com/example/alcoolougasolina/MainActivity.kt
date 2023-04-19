dcccdc7afd5c6325babaa7596aed7ed7e6f2891a
Rebim <102878183+levifmorais@users.noreply.github.com>
2023-04-19 19:54:39 -0300

    logica basica e mudanca no icone

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -5,60 +5,82 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var percentual: Double = 0.7
+    var percentual:Double = 0.7
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState != null) {
             percentual=savedInstanceState.getDouble("percentual")
         }
-        Log.d("PDM23", "No onCreate, $percentual")
+        Log.d("PDM23","No onCreate, $percentual")
 
         val btCalc: Button = findViewById(R.id.btCalcular)
         btCalc.setOnClickListener(View.OnClickListener {
             //código do evento
-            percentual = 0.75
-            Log.d("PDM23", "Na Inner Class, $percentual")
+            //percentual=0.75
+            val txtResultado: TextView = findViewById(R.id.textResultado)
+            val edAlcool= findViewById<EditText>(R.id.edAlcool).text.toString().toDoubleOrNull()
+            val edGas= findViewById<EditText>(R.id.edGasolina).text.toString().toDoubleOrNull()
+
+            if (edAlcool != null && edGas != null) {
+                if (edAlcool <= edGas * percentual) {
+                    txtResultado.text = "Álcool"
+                }
+                else {
+                    txtResultado.text = "Gasolina"
+                }
+            }
+            else {
+                txtResultado.text = "Valores inválidos"
+            }
+
+            Log.d("PDM23","No btCalcular, $percentual")
         })
 
+        val btSwitch: Switch = findViewById(R.id.swPercentual)
+        btSwitch.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked) {
+                percentual = 0.75
+            }
+            else {
+                percentual = 0.7
+            }
+            Log.d("PDM23","No Switch, $percentual")
         }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d("PDM23", "No onResume, $percentual")
     }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d("PDM23", "No onStart")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("PDM23", "No onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("PDM23", "No onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("PDM23", "No onDestroy")
-    }
-
-    fun onClickBtCalcular(v: View) {
+override fun onResume(){
+    super.onResume()
+    Log.d("PDM23","No onResume, $percentual")
+}
+override fun onStart(){
+    super.onStart()
+    Log.d("PDM23","No onResume")
+}
+override fun onPause(){
+    super.onPause()
+    Log.d("PDM23","No onResume")
+}
+override fun onStop(){
+    super.onStop()
+    Log.d("PDM23","No onResume")
+}
+override fun onDestroy(){
+    super.onDestroy()
+    Log.d("PDM23","No onResume")
+}
+fun onClickBtCalcular(v: View) {
         //código do evento
         percentual = 0.75
-        Log.d("PDM23", "No onClik, $percentual")
-    }
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putDouble("percentual",percentual)
+        Log.d("PDM23", "No onClick, $percentual")
+}
+override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-    }
-
-
-} // fim da classe
+        outState.putDouble("percentual",percentual)
+}
+}
